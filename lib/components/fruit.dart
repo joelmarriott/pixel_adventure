794fb955dfded61b6edd673af4b6c964500a31bf
Joel Marriott <joel.marriott.uk@gmail.com>
2026-08-26 20:47:56 +0100

    Fruit can be collided with and plays a collection animation

--- lib/components/fruit.dart
+++ lib/components/fruit.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/components/custom_hitbox.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
-class Fruit extends SpriteAnimationComponent with HasGameReference<PixelAdventure>{
+class Fruit extends SpriteAnimationComponent with HasGameReference<PixelAdventure>, CollisionCallbacks {
   final String fruit;
   Fruit(
     {this.fruit = 'Apple', position, size}
@@ -12,11 +14,20 @@ class Fruit extends SpriteAnimationComponent with HasGameReference<PixelAdventur
     size: size
   );
 
+  bool _collected = false;
   final double stepTime = 0.05;
+  final hitbox = CustomHitbox(offsetX: 10, offsetY: 10, width: 12, height: 12);
 
   @override
   FutureOr<void> onLoad() {
+    // debugMode = true;
     priority = -1;
+
+    add(RectangleHitbox(
+      position: Vector2(hitbox.offsetX, hitbox.offsetY),
+      size: Vector2(hitbox.width, hitbox.height),
+      collisionType: CollisionType.passive
+    ));
     animation = SpriteAnimation.fromFrameData(
       game.images.fromCache('Items/Fruits/$fruit.png'),
       SpriteAnimationData.sequenced(
@@ -28,4 +39,22 @@ class Fruit extends SpriteAnimationComponent with HasGameReference<PixelAdventur
     return super.onLoad();
   }
 
+  void collidedWithPlayer() {
+    if(!_collected) {
+      animation = SpriteAnimation.fromFrameData(
+        game.images.fromCache('Items/Fruits/Collected.png'),
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: stepTime,
+          textureSize: Vector2.all(32),
+          loop: false
+        )
+      );
+      _collected = true;
+    }
+    Future.delayed(
+      const Duration(milliseconds: 400),
+      () => removeFromParent()
+    );
+  }
 }
